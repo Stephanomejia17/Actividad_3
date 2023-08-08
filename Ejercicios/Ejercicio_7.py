8f3e5e95993aec76ebdc5bca49fbe5df50844b07
Stephanomejia17 <stephano.mejia@outlook.es>
2023-08-07 19:10:08 -0500

    Implementacion de codigo para Ejercicio_7

---
+++ Ejercicios/Ejercicio_7.py
@@ -0,0 +1,6 @@
+class CuentaBancaria:
+    def __init__(self, num_cuenta: str, propietarios: str, balance: str):
+        self.num_cuenta: str = num_cuenta
+        self.propietarios: str = propietarios
+        self.balance: str = balance
+
