d920bce13f5f1702da45149fa69a0c2e399bc01c
Stephanomejia17 <stephano.mejia@outlook.es>
2023-08-07 19:06:49 -0500

    Implementacion de codigo para Ejercicio_6

---
+++ Ejercicios/Ejercicio_6.py
@@ -0,0 +1,8 @@
+class Carta:
+    nombre_1 = "1"
+    nombre_2 = "2"
+    nombre_3 = "3"
+    nombre_4 = "4"
+    def __init__(self, valor: str, pinta:str ):
+        self.valor:str = valor
+        self.pinta:str = pinta
